b6815decac970a34f4b8b6a07916a50405b81125
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-01-01 16:12:30 +0100

    wharf: signature: iterate over file index instead of files

--- wharf/src/signature/verify.rs
+++ wharf/src/signature/verify.rs
@@ -1,8 +1,9 @@
 use super::read::Signature;
-use crate::common::{BLOCK_SIZE, apply_container_permissions, create_container_symlinks};
+use crate::common::{
+  BLOCK_SIZE, apply_container_permissions, create_container_symlinks, get_container_file_read,
+};
 use md5::{Digest, Md5};
 
-use std::fs;
 use std::io::Read;
 use std::path::Path;
 
@@ -19,29 +20,21 @@ impl Signature<'_> {
     let mut hasher = Md5::new();
 
     // Loop over all the files in the signature container
-    for container_file in &self.container_new.files {
-      let file_path = build_folder.join(&container_file.path);
+    for file_index in 0..self.container_new.files.len() {
       // Wrapping the file inside a BufReader isn't needed because
       // BLOCK_SIZE is already large
-      let mut file = fs::File::open(&file_path).map_err(|e| {
-        format!(
-          "Couldn't open file: \"{}\"\n{e}",
-          file_path.to_string_lossy()
-        )
-      })?;
+      let mut file = get_container_file_read(&self.container_new, file_index, build_folder)?;
 
       // Check if the file length matches
-      let metadata = file.metadata().map_err(|e| {
-        format!(
-          "Couldn't get file metadata: \"{}\"\n{e}",
-          file_path.to_string_lossy()
-        )
-      })?;
+      let file_size = self.container_new.files[file_index].size as u64;
 
-      if metadata.len() as i64 != container_file.size {
+      let metadata = file
+        .metadata()
+        .map_err(|e| format!("Couldn't get file metadata!\n{e}"))?;
+
+      if metadata.len() != file_size {
         return Err(format!(
-          "The signature and the in-disk size of \"{}\" don't match!",
-          file_path.to_string_lossy()
+          "The signature and the in-disk size of the file with index \"{file_index}\" don't match!",
         ));
       }
 
@@ -51,17 +44,13 @@ impl Signature<'_> {
       loop {
         // The size of the current block is BLOCK_SIZE,
         // unless there are less remaining bytes on the file
-        let current_block_size =
-          BLOCK_SIZE.min(container_file.size as u64 - block_index * BLOCK_SIZE);
+        let current_block_size = BLOCK_SIZE.min(file_size - block_index * BLOCK_SIZE);
 
         // Read the current block
         let buf = &mut buffer[..current_block_size as usize];
-        file.read_exact(buf).map_err(|e| {
-          format!(
-            "Couldn't read file data into buffer: \"{}\"\n{e}",
-            file_path.to_string_lossy()
-          )
-        })?;
+        file
+          .read_exact(buf)
+          .map_err(|e| format!("Couldn't read file data into buffer!\n{e}",))?;
 
         // Hash the current block
         hasher.update(buf);
@@ -86,7 +75,7 @@ impl Signature<'_> {
         progress_callback();
 
         // If the file has been fully read, proceed to the next one
-        if block_index * BLOCK_SIZE + current_block_size == container_file.size as u64 {
+        if block_index * BLOCK_SIZE + current_block_size == file_size {
           break;
         }
 
