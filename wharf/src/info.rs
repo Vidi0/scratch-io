de62d21e57e0c56bde6f88cdac8dc943dc8eb1bc
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-01-24 13:23:02 +0100

    wharf: add info module for identifying binaries

---
+++ wharf/src/info.rs
@@ -0,0 +1,21 @@
+use crate::common::{PATCH_MAGIC, SIGNATURE_MAGIC, read_magic_bytes};
+use crate::{Patch, Signature};
+
+use std::io::BufRead;
+
+pub enum WharfBinary<'a> {
+  Signature(Signature<'a>),
+  Patch(Patch<'a>),
+}
+
+/// Itentify a wharf binary based on the magic bytes and decode it
+pub fn identify<'a>(reader: &'a mut impl BufRead) -> Result<WharfBinary<'a>, String> {
+  use WharfBinary as WB;
+
+  let magic = read_magic_bytes(reader)?;
+  match magic {
+    SIGNATURE_MAGIC => Ok(WB::Signature(Signature::read_without_magic(reader)?)),
+    PATCH_MAGIC => Ok(WB::Patch(Patch::read_without_magic(reader)?)),
+    _ => Err("The provided binary doesn't match with any known wharf binary format".to_string()),
+  }
+}
--- wharf/src/lib.rs
+++ wharf/src/lib.rs
@@ -6,6 +6,9 @@ pub mod patch;
 /// Funcions and structures for reading wharf signatures
 pub mod signature;
 
+/// Identify the kind of wharf binary provided
+pub mod info;
+
 mod common;
 mod container;
 mod hasher;
