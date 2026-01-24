8e891fde9c0a4ca390777bcdfd12f2a764d6e8c0
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-01-24 14:17:35 +0100

    wharf: info: WharfBinary: add dump_stdout and print_summary

--- wharf/src/info.rs
+++ wharf/src/info.rs
@@ -8,6 +8,30 @@ pub enum WharfBinary<'a> {
   Patch(Patch<'a>),
 }
 
+impl WharfBinary<'_> {
+  /// Dump the inner binary’s contents to standard output
+  ///
+  /// This delegates to [`Signature::dump_stdout`] or
+  /// [`Patch::dump_stdout`], depending on the binary type.
+  pub fn dump_stdout(&mut self) -> Result<(), String> {
+    match self {
+      WharfBinary::Signature(s) => s.dump_stdout(),
+      WharfBinary::Patch(p) => p.dump_stdout(),
+    }
+  }
+
+  /// Print a simple summary of the inner binary’s contents to standard output
+  ///
+  /// This delegates to [`Signature::print_summary`] or
+  /// [`Patch::print_summary`], depending on the binary type.
+  pub fn print_summary(&self) {
+    match self {
+      WharfBinary::Signature(s) => s.print_summary(),
+      WharfBinary::Patch(p) => p.print_summary(),
+    }
+  }
+}
+
 /// Itentify a wharf binary based on the magic bytes and decode it
 pub fn identify<'a>(reader: &'a mut impl BufRead) -> Result<WharfBinary<'a>, String> {
   use WharfBinary as WB;
