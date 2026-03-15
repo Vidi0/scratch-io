cc85c9d3ad80634d330f6f5ad1064fc32e3ef70e
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-03-15 22:30:11 +0100

    itch_api: oauth: move things to constants

--- scratch-io/src/itch_api/oauth.rs
+++ scratch-io/src/itch_api/oauth.rs
@@ -4,8 +4,14 @@ pub use code_verifier::CodeVerifier;
 
 /// OAuth client ID used by the itch.io APP
 const CLIENT_ID: &str = "85252daf268d27fbefac93e1ac462bfd";
+const RESPONSE_TYPE: &str = "code";
+const SCOPE: &str = "itch";
+/// Percent-encoded form of `itch://oauth-callback`
 const REDIRECT_URI: &str = "itch%3A%2F%2Foauth-callback";
+/// itch.io OAuth 2.0 authorization endpoint
 const URL_ENDPOINT: &str = "https://itch.io/user/oauth";
+/// SHA-256 code challenge method, as defined in [RFC 7636 §4.2](https://datatracker.ietf.org/doc/html/rfc7636#section-4.2)
+const CODE_CHALLENGE_METHOD: &str = "S256";
 
 pub fn get_oauth_url() -> String {
   let code_verifier = code_verifier::CodeVerifier::random();
@@ -13,11 +19,11 @@ pub fn get_oauth_url() -> String {
   format!(
     "{URL_ENDPOINT}\
     ?client_id={CLIENT_ID}\
-    &scope=itch\
+    &response_type={RESPONSE_TYPE}\
+    &scope={SCOPE}\
     &redirect_uri={REDIRECT_URI}\
-    &response_type=code\
-    &code_challenge={}\
-    &code_challenge_method=S256",
+    &code_challenge_method={CODE_CHALLENGE_METHOD}\
+    &code_challenge={}",
     code_verifier.challenge()
   )
 }
