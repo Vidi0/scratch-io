353fee78db82e865cefc847daaaa2d24679e78a4
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-03-15 22:24:47 +0100

    itch_api: oauth: simplify const naming

--- scratch-io/src/itch_api/oauth.rs
+++ scratch-io/src/itch_api/oauth.rs
@@ -3,18 +3,18 @@ mod code_verifier;
 pub use code_verifier::CodeVerifier;
 
 /// OAuth client ID used by the itch.io APP
-const OAUTH_CLIENT_ID: &str = "85252daf268d27fbefac93e1ac462bfd";
-const OAUTH_REDIRECT_URI: &str = "itch%3A%2F%2Foauth-callback";
-const OAUTH_URL_ENDPOINT: &str = "https://itch.io/user/oauth";
+const CLIENT_ID: &str = "85252daf268d27fbefac93e1ac462bfd";
+const REDIRECT_URI: &str = "itch%3A%2F%2Foauth-callback";
+const URL_ENDPOINT: &str = "https://itch.io/user/oauth";
 
 pub fn get_oauth_url() -> String {
   let code_verifier = code_verifier::CodeVerifier::random();
 
   format!(
-    "{OAUTH_URL_ENDPOINT}\
-    ?client_id={OAUTH_CLIENT_ID}\
+    "{URL_ENDPOINT}\
+    ?client_id={CLIENT_ID}\
     &scope=itch\
-    &redirect_uri={OAUTH_REDIRECT_URI}\
+    &redirect_uri={REDIRECT_URI}\
     &response_type=code\
     &code_challenge={}\
     &code_challenge_method=S256",
