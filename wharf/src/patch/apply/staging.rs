985c442d87c30f854d7b82d7dfebd9db3e34cbf7
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-04-11 16:57:58 +0200

    wharf: patch: verify in parallel

--- wharf/src/patch/apply/staging.rs
+++ wharf/src/patch/apply/staging.rs
@@ -1,4 +1,4 @@
-use crate::hasher::BlockHasher;
+use crate::hasher::{BlockHasher, BlockHasherStatus};
 use crate::patch::operations::{
   apply::{self, FileCheckpoint, PatchFileStatus},
   skip::SkipStatus,
@@ -7,6 +7,8 @@ use crate::patch::{SyncEntryIter, SyncHeader};
 use crate::pool::{ContainerBackedPool, Pool, SeekablePool, StagingPool, WritablePool};
 
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::sync::mpsc::{self, Receiver, SyncSender};
 
 #[derive(Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
 #[must_use]
@@ -254,6 +256,90 @@ fn reconstruct_without_verification(
   })
 }
 
+/// File ready for verification
+#[derive(Debug)]
+struct FileToVerify {
+  file_index: usize,
+  reader: File,
+}
+
+/// Verification result from the verification thread
+#[derive(Debug)]
+struct VerificationResult {
+  file_index: usize,
+  status: BlockHasherStatus,
+}
+
+fn verify_files_thread(
+  hasher: &mut BlockHasher,
+  file_receiver: Receiver<FileToVerify>,
+  verification_status_sender: SyncSender<VerificationResult>,
+) -> Result<(), String> {
+  loop {
+    // Receive the next file to verify from the channel, blocking if necessary
+    let Ok(FileToVerify {
+      file_index,
+      mut reader,
+    }) = file_receiver.recv()
+    else {
+      // If the sender has disconnected, the verification has finished.
+      return Ok(());
+    };
+
+    // Verify the file
+    let status = hasher.hash_next_file(&mut reader, file_index, |_| ())?;
+
+    // Send the status through the channel back to the main thread
+    verification_status_sender
+      .send(VerificationResult { file_index, status })
+      .expect("The main thread must NOT hung up until receiving all the hasher status!");
+  }
+}
+
+fn handle_verification_results(
+  verification_status_receiver: &Receiver<VerificationResult>,
+  checkpoint: &mut StagingCheckpoint,
+  staging_pool: &mut StagingPool,
+  blocking: bool,
+) -> Result<(), String> {
+  let mut was_checkpoint_modified = false;
+
+  loop {
+    let hash_status = if blocking {
+      // In blocking mode, wait for all the verification status
+      match verification_status_receiver.recv() {
+        Ok(status) => status,
+        // The channel has been disconnected, so no more messages
+        // will be received
+        Err(_) => break,
+      }
+    } else {
+      // In non blocking mode, retrieve the verification status for
+      // the files that have already been verified
+      match verification_status_receiver.try_recv() {
+        Ok(status) => status,
+        // Don't block on empty channel, just return
+        Err(mpsc::TryRecvError::Empty) => break,
+        Err(mpsc::TryRecvError::Disconnected) => {
+          panic!("The hasher thread must NOT hung up when handling results in non blocking mode!")
+        }
+      }
+    };
+
+    // Check the status and store it into the checkpoint
+    let is_broken = hash_status.status.is_broken();
+
+    checkpoint.store_verification(hash_status.file_index, is_broken);
+    was_checkpoint_modified = true;
+  }
+
+  if was_checkpoint_modified {
+    staging_pool.save_checkpoint(checkpoint, false)?;
+  }
+
+  Ok(())
+}
+
 pub fn reconstruct_with_verification(
   src_pool: &mut (impl SeekablePool + ContainerBackedPool),
   staging_pool: &mut StagingPool,
@@ -263,41 +349,81 @@ pub fn reconstruct_with_verification(
   patch_op_buffer: &mut Vec<u8>,
   progress_callback: impl FnMut(u64) + Send,
 ) -> Result<ReconstructedFilesStatus, String> {
-  let on_file_patched = |info: PatchedFileInfo| {
+  // The sync op iterator hasn't been advanced yet, so the number of remaining
+  // entries is equal to the number of total files to patch
+  let files_to_patch = sync_op_iter.remaining_entries as usize;
+
+  let (file_sender, file_receiver) = mpsc::sync_channel::<FileToVerify>(files_to_patch);
+  let (verification_status_sender, verification_status_receiver) =
+    mpsc::sync_channel::<VerificationResult>(files_to_patch);
+
+  // Store the receiver as a reference
+  let verification_status_receiver = &verification_status_receiver;
+
+  // Move clousure to drop the file sender when it goes out of scope
+  //
+  // The verification status receiver is passed into the closure as a reference to allow using it
+  // later to finish the verficiation after patching all the files.
+  let on_file_patched = move |info: PatchedFileInfo| {
     // Update the checkpoint
     info.checkpoint.push_status(info.status);
 
-    // If the file has been patched, hash it
+    // If the file has been patched, send it though the channel in order for
+    // the hasher thread to hash it
     if let PatchFileStatus::Patched { .. } = info.status {
       // Get the reader
-      let mut reader = info.staging_pool.get_reader(info.file_index)?;
+      let reader = info.staging_pool.get_reader(info.file_index)?;
 
-      // Hash the file
-      let hash_status = hasher.hash_next_file(&mut reader, info.file_index, |_| ())?;
+      // Send the file to the hasher
+      file_sender
+        .send(FileToVerify {
+          file_index: info.file_index,
+          reader,
+        })
+        .expect("The hasher thread must NOT hung up until we do it!");
 
-      // Check the status and store it into the checkpoint
-      let is_broken = hash_status.is_broken();
-
-      info
-        .checkpoint
-        .store_verification(info.file_index, is_broken);
+      // Check if the hasher thread has finished verifying any new files
+      handle_verification_results(
+        verification_status_receiver,
+        info.checkpoint,
+        info.staging_pool,
+        false,
+      )?;
     }
 
-    // Save the checkpoint
-    info.staging_pool.save_checkpoint(&info.checkpoint, false)
+    Ok(())
   };
 
-  reconstruct_files_common(
-    src_pool,
-    staging_pool,
-    dst_pool,
-    sync_op_iter,
-    patch_op_buffer,
-    progress_callback,
-    on_file_patched,
-  )
-  .map(|checkpoint| ReconstructedFilesStatus {
-    patched_files: checkpoint.patched_files,
+  std::thread::scope(|scope| -> Result<ReconstructedFilesStatus, String> {
+    // Spawn the hasher thread
+    let hasher_handle =
+      scope.spawn(|| verify_files_thread(hasher, file_receiver, verification_status_sender));
+
+    // Patch the files, passing the verified ones to the hasher thread
+    let mut checkpoint = reconstruct_files_common(
+      src_pool,
+      staging_pool,
+      dst_pool,
+      sync_op_iter,
+      patch_op_buffer,
+      progress_callback,
+      on_file_patched,
+    )?;
+
+    // Check the verification status of the remaining files
+    handle_verification_results(
+      verification_status_receiver,
+      &mut checkpoint,
+      staging_pool,
+      true,
+    )?;
+
+    // Wait for the hasher thread to stop
+    hasher_handle.join().unwrap()?;
+
+    Ok(ReconstructedFilesStatus {
+      patched_files: checkpoint.patched_files,
+    })
   })
 }
 
