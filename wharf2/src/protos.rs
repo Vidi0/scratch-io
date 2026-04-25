34afb1bd4e234975b0d72c0d2a56944f58869b2c
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-04-25 16:36:25 +0200

    wharf2: more read_wharf_exact to binaries

--- wharf2/src/protos.rs
+++ wharf2/src/protos.rs
@@ -12,29 +12,13 @@ pub use bsdiff::*;
 pub use pwr::*;
 pub use tlc::*;
 
+use crate::binaries::read_wharf_exact;
 use crate::errors::{InvalidWharfBinary, IoError, Result};
 
 use std::io::{self, Read};
 
 /// <https://protobuf.dev/programming-guides/encoding/#varints>
 const PROTOBUF_VARINT_MAX_LENGTH: usize = 10;
-
-/// Reads the exact number of bytes required to fill `buf`.
-///
-/// Maps the error to return an [`InvalidWharfBinary::UnexpectedEOF`] error
-/// if an unexpected EOF was encountered, because calling this function means
-/// data was expected from the wharf binary.
-fn read_wharf_exact(reader: &mut impl Read, buf: &mut [u8]) -> Result<()> {
-  reader.read_exact(buf).map_err(|e| {
-    // Return an InvalidWharfBinary error if an unexpected EOF is encountered,
-    // and an IO error for every other case.
-    if let io::ErrorKind::UnexpectedEof = e.kind() {
-      InvalidWharfBinary::UnexpectedEOF(e).into()
-    } else {
-      IoError::WharfBinaryReadFailed(e).into()
-    }
-  })
-}
 
 /// Read a Protobuf length delimiter encoded as a variable-width integer and consume its bytes
 ///
