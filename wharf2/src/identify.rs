8c46e4ea0a21fc37b68eee5d5c5e2aa777d1183a
Vidi <125580894+Vidi0@users.noreply.github.com>
2026-05-01 15:57:21 +0200

    wharf2: identify: add WharfBinaryRead

--- wharf2/src/identify.rs
+++ wharf2/src/identify.rs
@@ -1,9 +1,13 @@
+use crate::binaries::signature::Signature;
+use crate::binaries::wounds::Wounds;
+use crate::binaries::{Dump, WharfBinary};
 use crate::errors::{InvalidWharfBinary, Result};
 use crate::magic::{
   MANIFEST_MAGIC, PATCH_MAGIC, SIGNATURE_MAGIC, WOUNDS_MAGIC, ZIP_INDEX_MAGIC, read_magic_bytes,
 };
 
-use std::io::Read;
+use std::fmt::Display;
+use std::io::{BufRead, Read};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum WharfBinaryKind {
@@ -12,6 +16,11 @@ pub enum WharfBinaryKind {
   Manifest,
   Wounds,
   ZipIndex,
+}
+
+pub enum WharfBinaryRead<'reader, R: BufRead> {
+  Signature(Signature<'reader, R>),
+  Wounds(Wounds<'reader, R>),
 }
 
 impl WharfBinaryKind {
@@ -27,5 +36,41 @@ impl WharfBinaryKind {
       ZIP_INDEX_MAGIC => Self::ZipIndex,
       magic => return Err(InvalidWharfBinary::MagicNotFound { found: magic }.into()),
     })
+  }
+
+  /// `reader` must *have* consumed its magic bytes
+  ///
+  /// # Panics
+  ///
+  /// The following binaries are not implemented yet: [`Self::Patch`], [`Self::Manifest`]
+  /// and [`Self::ZipIndex`].
+  ///
+  /// Therefore, calling this function on any of those variants will panic with a todo message.
+  pub fn read<R: BufRead>(self, reader: &mut R) -> Result<WharfBinaryRead<'_, R>> {
+    Ok(match self {
+      Self::Patch => todo!(),
+      Self::Signature => WharfBinaryRead::Signature(Signature::read_without_magic(reader)?),
+      Self::Manifest => todo!(),
+      Self::Wounds => WharfBinaryRead::Wounds(Wounds::read_without_magic(reader)?),
+      Self::ZipIndex => todo!(),
+    })
+  }
+}
+
+impl<R: BufRead> Dump for WharfBinaryRead<'_, R> {
+  fn dump(&mut self, writer: &mut impl std::io::Write) -> Result<()> {
+    match self {
+      Self::Signature(signature) => signature.dump(writer),
+      Self::Wounds(signature) => signature.dump(writer),
+    }
+  }
+}
+
+impl<R: BufRead> Display for WharfBinaryRead<'_, R> {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Self::Signature(signature) => write!(f, "{signature}"),
+      Self::Wounds(wounds) => write!(f, "{wounds}"),
+    }
   }
 }
